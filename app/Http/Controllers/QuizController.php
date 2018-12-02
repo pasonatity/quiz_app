c18a5e42d7366343a5a96d06060de9027c7b8f7f
pasonatity <pasonatity@gmail.com>
2018-12-02 14:47:25 +0900

    マイページにmiddlewareでauth認証を実装

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -13,6 +13,11 @@ use App\Http\Controllers\TopPageController;
 
 class QuizController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/QuizRedirectController.php
+++ app/Http/Controllers/QuizRedirectController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class QuizRedirectController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function storeRedirect()
     {
         return redirect(route('my_page_index'))->with('msg_save', '保存しました');
